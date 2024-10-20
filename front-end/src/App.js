29d2b2fd80d410410d83d1e72c76b6520368b763
Long <vulonggl11@gmail.com>
2024-10-20 14:07:22 +0700

    install new library

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import React from 'react'
+import {Route, Routes, Outlet} from 'react-router-dom';
 import './App.css';
-
-function App() {
+import { Box } from '@mui/material';
+import Home from './pages/Home';
+function Web() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Box width="400px" sx= {{ width: { xl: '1488px' }}} m = "auto">
+       <Route path="/" element={<Home />} />
+    </Box>
+  );  
 }
 
-export default App;
+export default Web
