3969032e01a97d9236d175401bb943753f5505bc
Long <vulonggl11@gmail.com>
2024-10-27 11:23:43 +0700

    update ProductController

--- back-end/app/Http/Controllers/ProductController.php
+++ back-end/app/Http/Controllers/ProductController.php
@@ -13,10 +13,18 @@ class ProductController extends Controller
         $product->name = $request->name;
         $product->price = $request->price;
         $product->description = $request->description;
-        $product->file_path =  $request->file('file')->store('products');
-        $product->file_path1 =  $request->file('file1')->store('products');
-        $product->file_path2 =  $request->file('file2')->store('products');
-        $product->file_path3 =  $request->file('file3')->store('products');
+        if ($request->file('file1')){
+            $product->file_path1 =  $request->file('file1')->store('products');
+        }
+        if ($request->file('file2')){
+            $product->file_path2 =  $request->file('file2')->store('products');
+        }
+        if ($request->file('file3')){
+            $product->file_path3 =  $request->file('file3')->store('products');
+        }
+        if ($request->file('file4')){
+            $product->file_path4 =  $request->file('file4')->store('products');
+        }
         $product->save();
         return $product;
     }
@@ -46,9 +54,6 @@ class ProductController extends Controller
             if ($request->has('name')) $product->name = $request->name;
             if ($request->has('price')) $product->price = $request->price;
             if ($request->has('description')) $product->description = $request->description;
-            if ($request->hasFile('file')) {
-                $product->file_path = $request->file('file')->store('products');
-            }
             if ($request->hasFile('file1')) {
                 $product->file_path1 = $request->file('file1')->store('products');
             }
@@ -57,6 +62,9 @@ class ProductController extends Controller
             }
             if ($request->hasFile('file3')) {
                 $product->file_path3 = $request->file('file3')->store('products');
+            }
+            if ($request->hasFile('file4')) {
+                $product->file_path4 = $request->file('file4')->store('products');
             }
             $product->save();
 
